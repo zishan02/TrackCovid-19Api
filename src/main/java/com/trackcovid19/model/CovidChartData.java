fafd582a5345630dc8d3f59e07994f1f19c2306c
Zishan <zishanmohammed02@gmail.com>
2020-04-19 17:48:24 +0530

    chart related changes

---
+++ src/main/java/com/trackcovid19/model/CovidChartData.java
@@ -0,0 +1,25 @@
+package com.trackcovid19.model;
+
+import java.util.List;
+
+public class CovidChartData {
+
+    private List<String> xAxis;
+    private List<Long> yAxis;
+
+    public List<String> getxAxis() {
+        return xAxis;
+    }
+
+    public void setxAxis(List<String> xAxis) {
+        this.xAxis = xAxis;
+    }
+
+    public List<Long> getyAxis() {
+        return yAxis;
+    }
+
+    public void setyAxis(List<Long> yAxis) {
+        this.yAxis = yAxis;
+    }
+}
