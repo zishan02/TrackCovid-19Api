1cb79d482e690b35e1b90909203445df17f644db
zishan <zishanmohammed02@gmail.com>
2020-04-21 23:15:50 +0530

    top5 changes

---
+++ src/main/java/com/trackcovid19/model/EstimatedCases.java
@@ -0,0 +1,23 @@
+package com.trackcovid19.model;
+
+public class EstimatedCases {
+
+    private Integer estimatedRecoveredWeek;
+    private Integer getEstimatedRecoveredMonth;
+
+    public Integer getEstimatedRecoveredWeek() {
+        return estimatedRecoveredWeek;
+    }
+
+    public void setEstimatedRecoveredWeek(Integer estimatedRecoveredWeek) {
+        this.estimatedRecoveredWeek = estimatedRecoveredWeek;
+    }
+
+    public Integer getGetEstimatedRecoveredMonth() {
+        return getEstimatedRecoveredMonth;
+    }
+
+    public void setGetEstimatedRecoveredMonth(Integer getEstimatedRecoveredMonth) {
+        this.getEstimatedRecoveredMonth = getEstimatedRecoveredMonth;
+    }
+}
