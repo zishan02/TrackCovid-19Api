16aa76cfe7c31d75ab3d5defccd087679f49dffb
zishan <zishanmohammed02@gmail.com>
2020-04-25 17:29:13 +0530

    Auto Chart Update

--- src/main/java/com/trackcovid19/model/CovidChartData.java
+++ src/main/java/com/trackcovid19/model/CovidChartData.java
@@ -1,25 +1,29 @@
 package com.trackcovid19.model;
 
+import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class CovidChartData {
+  @Id
+  private String id;
+  private ArrayList<String> xAxis;
+  private ArrayList<Long> yAxis;
 
-  private List<String> xAxis;
-  private List<Long> yAxis;
-
-  public List<String> getxAxis() {
+  public ArrayList<String> getxAxis() {
     return xAxis;
   }
 
-  public void setxAxis(List<String> xAxis) {
+  public void setxAxis(ArrayList<String> xAxis) {
     this.xAxis = xAxis;
   }
 
-  public List<Long> getyAxis() {
+  public ArrayList<Long> getyAxis() {
     return yAxis;
   }
 
-  public void setyAxis(List<Long> yAxis) {
+  public void setyAxis(ArrayList<Long> yAxis) {
     this.yAxis = yAxis;
   }
 }
