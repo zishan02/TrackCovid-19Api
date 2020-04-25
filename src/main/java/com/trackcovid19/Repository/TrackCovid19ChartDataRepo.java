bcfb255cec70af39f0ccb63701c3d697bf2100fe
zishan <zishanmohammed02@gmail.com>
2020-04-25 17:30:24 +0530

    Auto Chart Update

---
+++ src/main/java/com/trackcovid19/Repository/TrackCovid19ChartDataRepo.java
@@ -0,0 +1,8 @@
+package com.trackcovid19.Repository;
+
+import com.trackcovid19.model.CovidChartData;
+import com.trackcovid19.model.LastUpdated;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TrackCovid19ChartDataRepo extends MongoRepository<CovidChartData, String> {
+}
